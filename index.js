afa1e3ca971b44e8c035087f1b456356169b55f5
Power898 <limbo22city@gmail.com>
2023-07-09 22:43:20 +0200

    my homework js-2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Задача 1: Напишіть функцію, яка буде виконувати js код який передали в функцію через аргумент в форматі тексту
 
 // Створюємо функцію з назвою calculateExpression, яка приймає один аргумент expression
-function calculateExpression(expression) {
+function calculateExpression(expression) { if(isNaN(expression)) { return eval(expression);} return 'Argument is not a string';
   //Преревіряємо чи аргумент є рядком
   //Якщо так повертаємо виконананий код
   // Якщо ні повертаємо текст "Argument is not a string"
@@ -16,7 +16,8 @@ console.log(
 // Задача 2: Напишіть функцію, яка приймає рядок та повертає число з цього рядка або повідомлення про помилку.
 
 // Створюємо функцію з назвою getNumberFromString, яка приймає один аргумент s
-function getNumberFromString(s) {
+function getNumberFromString(s) {let value1=parseFloat(s); if(isNaN(value1)){ return 'Couldnt extract a number from the provided string';}
+ return value1;
   // Використовуємо функцію parseFloat для того, щоб спробувати отримати число з рядка
   // Перевіряємо, чи є отримане значення числом за допомогою isNaN
   // Якщо значення не є числом, повертаємо текст "Couldn't extract a number from the provided string."
@@ -37,7 +38,8 @@ console.log(
 // Задача 3: Напишіть функцію, яка приймає рядок та повертає ціле число з цього рядка або повідомлення про помилку.
 
 // Створюємо функцію з назвою getIntegerFromString, яка приймає один аргумент s
-function getIntegerFromString(s) {
+function getIntegerFromString(s) { let number1=parseInt(s); if(isNaN(number1)){return 'Coudent extract an integer from the provided string';}
+return number1;
   // Використовуємо функцію parseInt для того, щоб спробувати отримати ціле число з рядка
   // Перевіряємо, чи є отримане значення числом за допомогою isNaN
   // Якщо значення не є числом, повертаємо текст "Couldn't extract an integer from the provided string."
@@ -58,7 +60,7 @@ console.log(
 // Задача 4: Напишіть функцію, яка перевіряє, чи є введене число скінченним.
 
 // Створюємо функцію з назвою isNumberFinite, яка приймає один аргумент - num
-function isNumberFinite(num) {
+function isNumberFinite(num) {if(isFinite(num)) {return 'The number is finite';} return 'The number is not finite';
   // Використовуємо вбудовану функцію isFinite, щоб перевірити, чи є введене число скінченним.
   // Ця функція повертає true, якщо число є скінченним, і false, якщо число є нескінченним або не є числом.
   // За допомогою оператора if перевіряємо, чи є число скінченним.
@@ -74,7 +76,7 @@ console.log("isNumberFinite(Infinity)", isNumberFinite(Infinity)); // Вивед
 // Задача 5: Напишіть функцію, яка перевіряє, чи є введене значення NaN.
 
 // Створюємо функцію з назвою isValueNaN, яка приймає один аргумент - value
-function isValueNaN(value) {
+function isValueNaN(value) {if(isNaN(value)) {return 'The value is NaN';} return 'The value is not NaN';
   // Використовуємо вбудовану функцію isNaN, щоб перевірити, чи є введене значення NaN.
   // Ця функція повертає true, якщо значення є NaN, і false, якщо значення не є NaN.
   // За допомогою оператора if перевіряємо, чи є значення NaN.
@@ -90,7 +92,7 @@ console.log("isValueNaN(100)", isValueNaN(100)); // Виведе "The value is n
 // Задача 6: Напишіть функцію, яка створює об'єкт URL.
 
 // Створюємо функцію з назвою createURLObject, яка приймає один аргумент - urlStr
-function createURLObject(urlStr) {
+function createURLObject(urlStr) { return encodeURI(urlStr);
   // Використовуємо вбудований конструктор URL, щоб створити новий об'єкт URL з введеного рядка.
   // Повертаємо створений об'єкт URL.
 }
@@ -105,7 +107,7 @@ console.log(
 // Задача 7: Напишіть функцію, яка кодує компонент URL.
 
 // Створюємо функцію з назвою encodeURLComponent, яка приймає один аргумент - urlComponent
-function encodeURLComponent(urlComponent) {
+function encodeURLComponent(urlComponent) { return encodeURIComponent(urlComponent);
   // Використовуємо вбудовану функцію encodeURIComponent, щоб закодувати введений компонент URL.
   // Ця функція повертає закодований компонент URL, замінивши небезпечні символи на їх процентне кодування.
   // Повертаємо закодований компонент URL.
@@ -121,7 +123,7 @@ console.log(
 // Задача 8: Напишіть функцію, яка кодує URL.
 
 // Створюємо функцію з назвою encodeURL, яка приймає один аргумент - url
-function encodeURL(url) {
+function encodeURL(url) { return encodeURI(url);
   // Використовуємо вбудовану функцію encodeURI, щоб закодувати введений URL.
   // Ця функція повертає закодований URL, замінивши небезпечні символи на їх процентне кодування.
   // Повертаємо закодований URL.
@@ -137,7 +139,7 @@ console.log(
 // Задача 9: Напишіть функцію, яка декодує закодований компонент URL.
 
 // Створюємо функцію з назвою decodeURLComponent, яка приймає один аргумент - urlComponent
-function decodeURLComponent(urlComponent) {
+function decodeURLComponent(urlComponent) {return decodeURIComponent(urlComponent);
   // Використовуємо вбудовану функцію decodeURIComponent, щоб декодувати введений закодований компонент URL.
   // Ця функція повертає декодований компонент URL, замінивши процентне кодування символів на їх реальні значення.
   // Повертаємо декодований компонент URL.
@@ -153,7 +155,7 @@ console.log(
 // Задача 10: Напишіть функцію, яка декодує закодований URL.
 
 // Створюємо функцію з назвою decodeURL, яка приймає один аргумент - url
-function decodeURL(url) {
+function decodeURL(url) { return decodeURI(url);
   // Використовуємо вбудовану функцію decodeURI, щоб декодувати введений закодований URL.
   // Ця функція повертає декодований URL, замінивши процентне кодування символів на їх реальні значення.
   // Повертаємо декодований URL.
@@ -165,3 +167,4 @@ console.log(
   'decodeURL("https://ex%20ample.com")',
   decodeURL("https://ex%20ample.com")
 ); // Виведе "https://ex ample.com"
+    
